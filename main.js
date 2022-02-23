f0961799f1dde531ce47fbe402dba8e8def591fd
Kei Shimizu <suns.manage.k@gmail.com>
2022-02-23 17:33:50 +0900

    implemented some patterns

--- main.js
+++ main.js
@@ -4,16 +4,53 @@ const https = require('https');
 require('dotenv').config()
 
 class GoogleTrendsSlack {
-  constructor(date, geo) {
-    this.date = date;
-    this.geo = geo;
-
+  constructor() {
     this.eventEmitter = new EventEmitter();
     this.dailyData;
+    this.trendsData;
+    this.searchedData;
   }
 
   init() {
     this.eventEmitter.once('daily', () => this.sendDailyNotify());
+    this.eventEmitter.once('trends', () => this.sendTrendsNotify());
+    this.eventEmitter.once('search', () => this.sendSearchNotify());
+  }
+
+  daily(date, geo) {
+    googleTrends.dailyTrends({
+      trendDate: date,
+      geo
+    }, (err, res) => {
+      if (!err) {
+        this.dailyData = JSON.parse(res);
+        this.eventEmitter.emit('daily');
+      }
+    });
+  }
+
+  trends(geo, category) {
+    googleTrends.realTimeTrends({
+        geo,
+        category // All : 'all' Entertainment: 'e' Business : 'b' Science/Tech : 't' Health : 'm' Sports : 's' Top Stories : 'h'
+    },  (err, res) => {
+      if (!err) {
+        this.trendsData = JSON.parse(res);
+        this.eventEmitter.emit('trends');
+      }
+    });
+  }
+
+  search(keyword, hl) {
+    googleTrends.autoComplete({
+      keyword,
+      hl
+    },  (err, res) => {
+      if (!err) {
+        this.searchedData = JSON.parse(res);
+        this.eventEmitter.emit('search');
+      }
+    });
   }
 
   sendDailyNotify() {
@@ -57,28 +94,50 @@ class GoogleTrendsSlack {
       }
     }
 
-    const promise = this.sendSlackNotification(slackSection.slice(0, 49), "daily"); // 50件までしかnotifyできない
+    const promise = this.sendSlackNotification(slackSection, "Daily");
     Promise.resolve(promise).then((res) => console.log(res));
   }
 
-  daily() {
-    const res = googleTrends.dailyTrends({
-      trendDate: this.date,
-      geo: this.geo
-    }, (err, res) => {
-      if (!err) {
-        this.dailyData = JSON.parse(res);
-        this.eventEmitter.emit('daily');
+  sendTrendsNotify() {
+    const trends = this.trendsData;
+    const stories = trends.storySummaries.trendingStories;
+
+    const slackSection = [{
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text": "Realtime Google Trends"
       }
-    });
+    }];
+
+    for (const storyKey in stories) {
+      const story = stories[storyKey];
+      const firstArticle = story.articles['0'];
+
+      slackSection.push({
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `<${firstArticle.url}|${firstArticle.articleTitle}>\n<${story.shareUrl}|google trends>`
+        }
+      })
+    }
+
+    const promise = this.sendSlackNotification(slackSection, "Realtime"); 
+    Promise.resolve(promise).then((res) => console.log(res));
+  }
+
+  sendSearchNotify() {
+    const search = this.searchedData;
+    console.log(search.default.topics);
   }
 
   sendSlackNotification(slackSection, trendType) {
     const messageWithMeta = {
       'username': 'Google Trends',
-      'text': `Daily | Google Trends`,
+      'text': `${trendType} | Google Trends`,
       'icon_emoji': ':ghost:',
-      "blocks": slackSection
+      "blocks": slackSection.slice(0, 49) // 50件までしかnotifyできない
     }
 
     let messageBody;
@@ -118,25 +177,76 @@ class GoogleTrendsSlack {
 
 
 function notifyDailyJS () {
-  const date = new Date();
-  const obj = new GoogleTrendsSlack(date, geo='JP');
-
+  const obj = new GoogleTrendsSlack();
   obj.init();
-  obj.daily();
+
+  const date = new Date();
+  obj.daily(date, geo='JP');
 }
 
 function notifyDailyUS () {
-  const date = new Date();
-  const obj = new GoogleTrendsSlack(date, geo='US');
-
+  const obj = new GoogleTrendsSlack();
   obj.init();
-  obj.daily();
+
+  const date = new Date();
+  obj.daily(date, geo='US');
+}
+
+function notifyTrendsJSBusiness () {
+  const obj = new GoogleTrendsSlack();
+  obj.init();
+
+  obj.trends(geo='JP', category='b');
+}
+
+function notifyTrendsJSTech () {
+  const obj = new GoogleTrendsSlack();
+  obj.init();
+
+  obj.trends(geo='JP', category='t');
+}
+
+function notifyTrendsUS () { // あまり役に立たない
+  const obj = new GoogleTrendsSlack();
+  obj.init();
+
+  obj.trends(geo='US', category='b');
+}
+
+function search() {
+  const obj = new GoogleTrendsSlack();
+  obj.init();
+
+  obj.search(keyword="metaverse", hl='en');
 }
 
 
 function main() {
-  notifyDailyJS();
-  // notifyDailyUS();
+  process.argv.forEach((val, index) => {
+    if (index && index === 2) {
+      switch (val) {
+        case 'dj':
+          notifyDailyJS();
+          break
+        case 'du':
+          notifyDailyUS();
+          break
+        case 'tjb':
+          notifyTrendsJSBusiness();
+          break
+        case 'tje':
+          notifyTrendsJSTech();
+          break
+        case 'tu':
+          notifyTrendsUS();
+          break
+        default:
+          notifyTrendsJSBusiness();
+      }
+    }    
+  });
+
+  // search();
 }
 
 main();
